57bcc4bc779ee72e2fbd5649b013fc5d743aa85e
iloveYou <i@blackrabbit.info>
2018-12-22 05:44:47 +0200

    str helper lib stub

---
+++ src/lib/str.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace alexandria\lib;
+
+class str
+{
+    /**
+     * Adds 's' suffix for the nouns by the number
+     * @param  int    $number           [description]
+     * @param  string $single_variant   [description]
+     * @param  [type] $multiple_variant [description]
+     * @return [type]                   [description]
+     */
+    public static function numstr(int $number, string $noun)
+    {
+        $ret = "{$number} {$noun}";
+        if ($number % 10 != 1) {
+            $ret .= 's';
+        }
+
+        return $ret;
+    }
+}
