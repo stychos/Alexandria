c94b66fd989f346c4c0ee10f42e4c196651681cc
iloveYou <i@blackrabbit.info>
2018-12-25 07:07:34 +0200

    noescape flag: !

--- src/lib/form.php
+++ src/lib/form.php
@@ -26,10 +26,10 @@ class form
      *
      * @param string $__file Filename of the form file to load.
      * @param mixed  $__vars Variables to pass to the form.
-     *
+     * @param bool   $__mute Hide source templates if variable was not found.
      * @return string Loaded form content.
      */
-    public static function load(string $__file, array $__vars = []): string
+    public static function load(string $__file, array $__vars = [], bool $__mute = false): string
     {
         if (!file_exists($__file)) {
             throw new \RuntimeException("Can not load form: {$__file}");
@@ -45,28 +45,40 @@ class form
 
         // Replace content mnemonics, in example: {$var} will become to $var value, if found (and scalar) in passed variables.
         // Also, {CONST} will become to CONST constant value, if constant defined.
-        preg_match_all('/\{\$?(?<name>[a-zA-Z_\x7f-\xff][\->\[\]\'"a-zA-Z0-9_\x7f-\xff]*)\}/', $__content, $__to_replace);
-        if (is_array($__to_replace[0]) && !empty($__to_replace[0])) {
+        preg_match_all('/\{(?<operation>[\$!])(?<name>[a-zA-Z_\x7f-\xff][\->\[\]\'"a-zA-Z0-9_\x7f-\xff]*)\}/', $__content, $__to_replace);
+        if (!empty($__to_replace[0])) {
             foreach (array_unique($__to_replace['name']) as $__index => $__var) {
                 $__src = $__to_replace[0][$__index];
+                $__esc = $__to_replace['operation'][$__index] == '$';
+                $__res = false;
 
                 // Check in extracted scalar variables
                 if (isset($__vars[$__var]) && is_scalar($__vars[$__var])) {
-                    $__content = str_replace($__src, htmlspecialchars($__vars[$__var]), $__content);
+                    $__res = $__vars[$__var];
                 }
 
                 // Otherwise, try to find appropriate constant, always scalar
                 elseif (defined($__var)) {
-                    $__content = str_replace($__src, htmlspecialchars(constant($__var)), $__content);
+                    $__res = $__var;
                 }
 
                 // Check for objects
                 else {
-                    $__var = preg_replace('~\{\$?(.+)\}~', '$1', $__src);
-                    $__esrc = addslashes($__src);
-                    $__val = eval("return \${$__var} ?? '{$__esrc}';");
-                    $__content = str_replace($__src, htmlspecialchars($__val), $__content);
+                    $__var = preg_replace('~\{[\$!]?(.+)\}~', '$1', $__src);
+                    $__evar = addslashes($__src);
+                    $__val = eval("return \${$__var} ?? '{$__evar}';");
+                    $__res = $__val;
                 }
+
+                if ($__esc) {
+                    $__res = htmlspecialchars($__res);
+                }
+
+                if (is_null($__res) && $__mute == false) {
+                    $__res = $__src;
+                }
+
+                $__content = str_replace($__src, $__res, $__content);
             }
         }
 
