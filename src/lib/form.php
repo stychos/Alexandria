ccd772778bf3ff3627eb69a949bddb6fbf52508e
iloveYou <i@blackrabbit.info>
2018-12-26 08:39:09 +0200

    render command handling improved, dropped mute argument

--- src/lib/form.php
+++ src/lib/form.php
@@ -24,12 +24,15 @@ class form
      * Note: if form variables and passed variables have similar names, form variables will overwrite passed variables
      * Also, it is VERY STRONGLY recommended to NOT USE "__form" as variable name in the forms
      *
+     * {$var} -> escaped $var content or unchanged if $var not found
+     * {?var} -> escaped $var content or empty if $var not found
+     * {!var} -> unescaped $var content or empty if $var not found
+     *
      * @param string $__file Filename of the form file to load.
      * @param mixed  $__vars Variables to pass to the form.
-     * @param bool   $__mute Hide source templates if variable was not found.
      * @return string Loaded form content.
      */
-    public static function load(string $__file, array $__vars = [], bool $__mute = false): string
+    public static function load(string $__file, array $__vars = []): string
     {
         if (!file_exists($__file)) {
             throw new \RuntimeException("Can not load form: {$__file}");
@@ -45,49 +48,58 @@ class form
 
         // Replace content mnemonics, in example: {$var} will become to $var value, if found (and scalar) in passed variables.
         // Also, {CONST} will become to CONST constant value, if constant defined.
-        preg_match_all('/\{(?<operation>[\$!])(?<name>[a-zA-Z_\x7f-\xff][\->\[\]\'"a-zA-Z0-9_\x7f-\xff]*)\}/', $__content, $__to_replace);
-        if (!empty($__to_replace[0])) {
-            foreach (array_unique($__to_replace['name']) as $__index => $__var) {
-                $__src = $__to_replace[0][$__index];
-                $__esc = $__to_replace['operation'][$__index] == '$';
-                $__res = false;
+        preg_match_all('/\{(?<operation>[\$\?\!])(?<name>[a-zA-Z_\x7f-\xff][\->\(\)\[\]\'"a-zA-Z0-9_\x7f-\xff]*)\}/', $__content, $__to_replace);
+        foreach (array_unique($__to_replace[0]) as $__index => $__src) {
+            $__ret = null;
+            $__cmd = $__to_replace['operation'][$__index];
+            $__var = $__to_replace['name'][$__index];
 
-                // Check in extracted scalar variables
-                if (isset($__vars[$__var]) && is_scalar($__vars[$__var])) {
-                    $__res = $__vars[$__var];
+            // Check in extracted scalar variables
+            if (isset($__vars[$__var])) {
+                $__tmp = $__vars[$__var];
+                if (is_scalar($__tmp)) {
+                    $__ret = $__tmp;
                 }
 
-                // Otherwise, try to find appropriate constant, always scalar
-                elseif (defined($__var)) {
-                    $__res = $__var;
+                // Check in objects contains __toString()
+                elseif (is_object($__tmp) && method_exists($__tmp, '__toString')) {
+                    $__ret = (string) $__tmp;
                 }
-
-                // Check for objects
-                else {
-                    $__var = preg_replace('~\{[\$!]?(.+)\}~', '$1', $__src);
-                    $__evar = addslashes($__src);
-                    $__val = eval("return \${$__var} ?? '{$__evar}';");
-                    $__res = $__val;
-                }
-
-                if ($__esc) {
-                    $__res = htmlspecialchars($__res);
-                }
-
-                if (is_null($__res) && $__mute == false) {
-                    $__res = $__src;
-                }
-
-                $__content = str_replace($__src, $__res, $__content);
             }
+
+            // check in constants
+            elseif (defined($__var)) {
+                $__ret = $__var;
+            }
+
+            // check in object methods and arrays
+            else {
+                try {
+                    $__tmp = @eval("return \${$__var};");
+                }
+                catch (\Throwable $e) {
+                    $__tmp = null;
+                }
+                $__ret = $__tmp;
+            }
+
+            if ($__cmd != '!') {
+                if (is_null($__ret) && $__cmd == '$') {
+                    $__ret = $__src;
+                }
+
+                $__ret = htmlspecialchars($__ret);
+            }
+
+            $__content = str_replace($__src, $__ret, $__content);
         }
 
         // Ret-Evaluations
         preg_match_all('/<=\s*(?<eval>.+)=>/u', $__content, $__to_replace);
         if (is_array($__to_replace[0]) && !empty($__to_replace[0])) {
             foreach (array_unique($__to_replace['eval']) as $__index => $__cmd) {
-                $__src = $__to_replace[0][$__index];
-                $__val = eval("return {$__cmd};");
+                $__src     = $__to_replace[0][$__index];
+                $__val     = eval("return {$__cmd};");
                 $__content = str_replace($__src, htmlspecialchars($__val), $__content);
             }
         }
