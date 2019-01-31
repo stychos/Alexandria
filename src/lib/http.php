d4fb762def440d86246f0f3f0af1b2c6eb2eadf6
iloveYou <i@blackrabbit.info>
2019-01-31 09:04:06 +0200

    cleaned up path detection code

--- src/lib/http.php
+++ src/lib/http.php
@@ -11,8 +11,25 @@ class http
 
     public function __construct()
     {
-        $proto = @$_SERVER['HTTPS'] && $_SERVER['HTTPS'] != 'off' || @$_SERVER['SERVER_PORT'] == 443 ? 'https' : 'http';
-        $wroot = @$_SERVER['HTTP_HOST'] ? $proto.'://'.rtrim($_SERVER['HTTP_HOST'], '/') : '';
+        $proto = 'http';
+        if (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] != 'off'
+        || !empty($_SERVER['SERVER_PORT']) && $_SERVER['SERVER_PORT'] == 443)
+        {
+            $proto = 'https';
+        }
+
+        $wroot = '';
+        if (!empty($_SERVER['HTTP_HOST']))
+        {
+            $wroot = "{$proto}://{$_SERVER['HTTP_HOST']}";
+            $wroot = rtrim($wroot, '/');
+        }
+
+        if (empty($_SERVER['PATH_INFO']))
+        {
+            $_SERVER['PATH_INFO'] = '';
+        }
+
         $sub   = preg_replace("#(/index\.php)?{$_SERVER['PATH_INFO']}#", '', $_SERVER['PHP_SELF']);
         $wroot .= !empty($sub) ? "{$sub}" : '';
 
